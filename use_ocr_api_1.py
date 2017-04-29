d157df337444d726ac8a049a50b6756b2e670a99
mateusz <mateusz.jurewicz@gmail.com>
2017-04-29 19:14:54 +0200

    Used an API to take links to images and read parsed text

---
+++ use_ocr_api_1.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+def ocr_space_file(filename, overlay=False, api_key='a2b94ede3488957', language='eng'):
+    """ OCR.space API request with local file.
+        Python3.5 - not tested on 2.7
+    :param filename: Your file path & name.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    with open(filename, 'rb') as f:
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          )
+    return r.content.decode()
+
+
+def ocr_space_url(url, overlay=False, api_key='a2b94ede3488957', language='eng'):
+    """ OCR.space API request with remote file.
+        Python3.5 - not tested on 2.7
+    :param url: Image url.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'url': url,
+               'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    r = requests.post('https://api.ocr.space/parse/image',
+                      data=payload,
+                      )
+    return r.content.decode()
+
+
+# Use examples:
+test_file = ocr_space_file(filename='How-to-Read-Your-Meter.jpg', language='pol')
+test_url = ocr_space_url(url='http://www.smarthomewaterguide.org/images/water_meter_analog.jpg')
+
+json1_data = json.loads(test_url)
+
+# their json structure is weeeeird...
+print(json1_data['ParsedResults'][0]['ParsedText'])
