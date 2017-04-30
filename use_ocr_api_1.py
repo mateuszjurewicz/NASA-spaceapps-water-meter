409c77b0c4c0e9c770f64699c452c23fc1f67fe0
mateusz <mateusz.jurewicz@gmail.com>
2017-04-30 17:32:28 +0200

    Added command-line argument handling

--- use_ocr_api_1.py
+++ use_ocr_api_1.py
@@ -1,15 +1,22 @@
+# Python 3.6.1
+# GRABS A LINK TO AN IMAGE AND USES EXTERNAL OCR API TO RETURN THE TEXT
+# PROVIDE THE LINK AS FIRST COMMAND LINE ARGUMENT
+
+# Works on this example input:
+# http://www.smarthomewaterguide.org/images/water_meter_analog.jpg
+
+
 import requests
 import json
 
-
 def ocr_space_file(filename, overlay=False, api_key='a2b94ede3488957', language='eng'):
     """ OCR.space API request with local file.
-        Python3.5 - not tested on 2.7
+        Python3.6
     :param filename: Your file path & name.
     :param overlay: Is OCR.space overlay required in your response.
                     Defaults to False.
     :param api_key: OCR.space API key.
-                    Defaults to 'helloworld'.
+                    Defaults to current key (500 usage)
     :param language: Language code to be used in OCR.
                     List of available language codes can be found on https://ocr.space/OCRAPI
                     Defaults to 'en'.
@@ -30,12 +37,12 @@ def ocr_space_file(filename, overlay=False, api_key='a2b94ede3488957', language=
 
 def ocr_space_url(url, overlay=False, api_key='a2b94ede3488957', language='eng'):
     """ OCR.space API request with remote file.
-        Python3.5 - not tested on 2.7
+        Python3.6
     :param url: Image url.
     :param overlay: Is OCR.space overlay required in your response.
                     Defaults to False.
     :param api_key: OCR.space API key.
-                    Defaults to 'helloworld'.
+                    Defaults to current key (500 usage)
     :param language: Language code to be used in OCR.
                     List of available language codes can be found on https://ocr.space/OCRAPI
                     Defaults to 'en'.
@@ -54,14 +61,18 @@ def ocr_space_url(url, overlay=False, api_key='a2b94ede3488957', language='eng')
 
 
 if __name__ == '__main__':
-    # Working example:
-    # http: // www.smarthomewaterguide.org / images / water_meter_analog.jpg
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("linked_image", help="echo the image-link you wish to use")
+    args = parser.parse_args()
 
     # Use examples:
     # test_file = ocr_space_file(filename='How-to-Read-Your-Meter.jpg', language='pol')
-    test_url = ocr_space_url(url='http://www.smarthomewaterguide.org/images/water_meter_analog.jpg')
+    test_url = ocr_space_url(args.linked_image)
     # load the string to json
     json1_data = json.loads(test_url)
 
     # their json structure is weeeeird...
-    print(json1_data['ParsedResults'][0]['ParsedText'])
+    print(json1_data['ParsedResults'][0]['ParsedText'][:4])
